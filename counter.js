b27932c73e3082d9b278fc651eb224b8fcd12e3c
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-03-05 23:49:15 +0500

    Update counter.js

--- counter.js
+++ counter.js
@@ -5,3 +5,7 @@ var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
 var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+
+
+console.log(days)
