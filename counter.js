acf41823398a67375b7e655f1de12389a6f247ec
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-03-11 17:01:19 +0500

    Update counter.js

--- counter.js
+++ counter.js
@@ -8,3 +8,17 @@ var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
 document.write(days + "<br>") 
 document.write(hours + "<br>") 
+
+const expr = 'Papayas';
+switch (expr) {
+  case 'Oranges':
+    console.log('Oranges are $0.59 a pound.');
+    break;
+  case 'Mangoes':
+  case 'Papayas':
+    console.log('Mangoes and papayas are $2.79 a pound.');
+    // Expected output: "Mangoes and papayas are $2.79 a pound."
+    break;
+  default:
+    console.log(`Sorry, we are out of ${expr}.`);
+}
