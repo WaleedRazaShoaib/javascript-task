3c8c35a21a5e2ff86aae5d21db441caad8ee0bd7
WaleedRazaShoaib <146237300+WaleedRazaShoaib@users.noreply.github.com>
2024-03-12 17:08:24 +0500

    Update counter.js

--- counter.js
+++ counter.js
@@ -23,3 +23,26 @@ switch (expr) {
   default:
     console.log(`Sorry, we are out of ${expr}.`);
 }
+
+switch (new Date().getDay()) {
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+  }
